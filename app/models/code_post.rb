cbd17576083889f646b5d456a67df24b4062b2bc
Ugnius Skucas <eparis@atlassian.com>
2016-07-10 19:07:06 +0300

    Remove default method from CodePost Model

--- app/models/code_post.rb
+++ app/models/code_post.rb
@@ -1,13 +1,6 @@
 class CodePost < ActiveRecord::Base
 
   validates_presence_of [:title, :code, :category, :code_language]
-
-  after_initialize :set_defaults # Set defaults after creation
   scope :available, -> { where(deleted_at: nil) } # Not deleted CodePost scope
 
-  private
-    def set_defaults
-      self.repup ||= 0
-      self.repdown ||= 0
-    end
 end
