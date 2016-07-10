19ce12d471b0f51f9725fb514b5978061fa4725d
Ugnius Skucas <eparis@atlassian.com>
2016-07-10 15:41:45 +0300

    Programming languages for code posts

--- app/models/code_language.rb
+++ app/models/code_language.rb
@@ -3,4 +3,12 @@ class CodeLanguage
   def self.generate_languages(count = 1)
     Faker::Lorem.words(count, true)
   end
+
+  def self.get_languages
+    %w[c c++ c# java ruby python php javascript swift objective-c]
+  end
+
+  def self.count
+    10
+  end
 end
