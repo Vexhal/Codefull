6ba3951abc025480092898df0bb28e96040ab56f
Ugnius Skucas <eparis@atlassian.com>
2016-07-10 15:46:44 +0300

    Language generator fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,12 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 categories = Category.generate_categories(10)
-code_languages = CodeLanguage.generate_languages(10)
 
 20.times do |i|
-  CodePost.create(title: Faker::Company.name,
-                   code: Faker::Lorem.paragraph(10),
-                   category: categories[rand(0..10)],
-                   code_language: code_languages[rand(0..10)],
-                   user_id: 1)
+  CodePost.create(
+    title: Faker::Company.name,
+    code: Faker::Lorem.paragraph(10),
+    category: categories[rand(0..9)],
+    code_language: CodeLanguage.get_languages[rand(0..(CodeLanguage.count - 1))],
+    user_id: 1)
 end
