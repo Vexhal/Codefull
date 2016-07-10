b7f285d8479b3264556a7b39a4f8f5431b30ccce
Ugnius Skucas <eparis@atlassian.com>
2016-07-10 04:14:00 +0300

    Default user type through migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,4 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  after_initialize :set_defaults # Setting defaults after initialization
-
-  private
-    def set_defaults
-      self.user_type = "user"
-    end
 end
---
+++ db/migrate/20160710011037_change_user_type_for_user.rb
@@ -0,0 +1,5 @@
+class ChangeUserTypeForUser < ActiveRecord::Migration
+  def change
+    change_column :users, :user_type, :string, default: "user"
+  end
+end
