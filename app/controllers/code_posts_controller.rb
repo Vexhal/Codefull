785a1c0db07a09738431d4d885670dfe110c99e0
Ugnius Skucas <eparis@atlassian.com>
2016-07-10 16:34:05 +0300

    Authentication for editing Code Post

--- app/controllers/code_posts_controller.rb
+++ app/controllers/code_posts_controller.rb
@@ -4,10 +4,11 @@ class CodePostsController < ApplicationController
   before_action :set_code_post, only: [:show, :edit]
 
   def index
-    @code_posts = CodePost.available.all
+    @code_posts = CodePost.available.all # Get all non deleted Code posts
   end
 
   def published
+    # Get all non deleted and published by owner Code Posts
     @code_posts = CodePost.available.where(user_id: current_user).all
     render 'index' # Render default view
   end
@@ -16,16 +17,20 @@ class CodePostsController < ApplicationController
   end
 
   def new
-    @code_post = CodePost.new
+    @code_post = CodePost.new # New Code Post
   end
 
   def edit
+    if is_owner
+
+    else
+      redirect_to root_path
+    end
   end
 
   def create
     @code_post = CodePost.new(code_post_params)
     @code_posts.user_id = current_user # Setting whom will belong to
-
     respond_to do |format|
       if @code_post.save
         format.html { redirect_to @task, notice: "Code created successfully!"}
@@ -38,8 +43,6 @@ class CodePostsController < ApplicationController
   end
 
   def update
-    @code_posts.user_id = current_user # Setting whom will belong to
-
     respond_to do |format|
       if @code_post.update(code_post_params)
         format.html { redirect_to @task, notice: "Code updated successfully!"}
@@ -62,10 +65,17 @@ class CodePostsController < ApplicationController
 
   private
 
+    # Is Owner of the Code Post?
+    def is_owner?
+      @code_post.user_id == current_user
+    end
+
+    # Setting code_post before action
     def set_code_post
       @code_post = CodePost.find(params[:id])
     end
 
+    # Strong params
     def code_post_params
       params.require(:code_posts).permit(:title, :code, :repup, :repdown,
       :category, :code_language, :user_id)
